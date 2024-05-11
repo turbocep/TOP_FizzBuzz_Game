36ed717380ab347727c72e0a43919b055101358c
turbocep <162988238+turbocep@users.noreply.github.com>
2024-05-11 16:47:09 +0200

    create while loop that creates FizzBuzz string

--- scripts.js
+++ scripts.js
@@ -4,3 +4,20 @@ function getUserInput() {
 
 let userInput = getUserInput();
 
+let i = 1;
+let finalString = "";
+
+while(i <= userInput) {
+  if(i % 3 == 0 && i % 5 == 0) {
+    finalString += " FizzBuzz";
+  } else if (i % 3 == 0) {
+    finalString += " Fizz";
+  } else if (i % 5 == 0) {
+    finalString += " Buzz";
+  } else {
+    finalString += ` ${i}`;
+  }
+  i++;
+}
+
+alert(finalString);
