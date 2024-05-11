864497cdf16e3351f4d1c2a3c9c217bed8a6d898
turbocep <162988238+turbocep@users.noreply.github.com>
2024-05-11 16:51:04 +0200

    create getFizzBuzz function.
    The getFizzBuzz function was implemented to
    minimize the number of global variables required
    and simplify the code. It takes a number as a
    parameter and returns "FizzBuzz", "Fizz", "Buzz"
    or the number based on conditions outlined in the
    readme.

--- scripts.js
+++ scripts.js
@@ -21,3 +21,15 @@ while(i <= userInput) {
 }
 
 alert(finalString);
+
+function getFizzBuzz(num) {
+  if(num % 3 == 0 && num % 5 == 0) {
+    return "FizzBuzz"
+  } else if (num % 3 == 0) {
+    return "Fizz"
+  } else if (num % 5 == 0) {
+    return "Buzz"
+  } else {
+    return num;
+  }
+}
